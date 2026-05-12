612e07b275982af465062761fd8036a29b3f5f8d
adarsha-probestack <adarsha.maharana@probestack.io>
2026-05-12 16:07:57 +0530

    Push from ForgeSphere MCP wizard

---
+++ src/tools/index.ts
@@ -0,0 +1,4 @@
+// Re-export all tool handlers.
+export { create_taskHandler } from "./create_task.js";
+export { list_tasksHandler } from "./list_tasks.js";
+export { complete_taskHandler } from "./complete_task.js";
